285ee1b56ef7185912ab776d279d34da1e0c7cd0
Julian Jelfs <julian.jelfs@gmail.com>
2025-06-01 11:55:04 +0100

    add poll function for regular refresh

---
+++ src/poll.ts
@@ -0,0 +1,11 @@
+import type { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { subscriptions } from "./subscriptions";
+
+export const poll: APIGatewayProxyHandlerV2 = async (_) => {
+  await subscriptions.initialising;
+  await subscriptions.refresh();
+  console.log("Refreshed all subscriptions");
+  return {
+    statusCode: 200,
+  };
+};
