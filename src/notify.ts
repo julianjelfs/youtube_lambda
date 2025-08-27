1b11fcae4c8dc1434d7e75004b76da9079bf0cb3
Julian Jelfs <julian.jelfs@gmail.com>
2025-08-27 10:29:58 +0100

    upgrade to bot sdk 60

--- src/notify.ts
+++ src/notify.ts
@@ -1,44 +1,51 @@
 import {
-    InstallationRecord,
-    parseBotNotification,
+  BotClient,
+  BotEvent,
+  handleNotification,
+  InstallationRecord,
 } from "@open-ic/openchat-botclient-ts";
 import type { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { saveInstallation, uninstall, withPool } from "./db/database";
+import { factory } from "./factory";
 
 export const notify: APIGatewayProxyHandlerV2 = async (event) => {
-  const result = parseBotNotification(event.body);
-  if (result.kind === "bot_event_wrapper") {
-    if (result.event.kind === "bot_installed_event") {
-      console.log("installing: ", result.event.location);
-      const location = result.event.location;
-      const record = new InstallationRecord(
-        result.apiGateway,
-        result.event.grantedAutonomousPermissions,
-        result.event.grantedCommandPermissions
-      );
+  return handleNotification(
+    event.body!,
+    factory,
+    async (client: BotClient, ev: BotEvent, apiGateway: string) => {
+      if (ev.kind === "bot_installed_event") {
+        console.log("installing: ", ev.location);
+        const location = ev.location;
+        const record = new InstallationRecord(
+          apiGateway,
+          ev.grantedAutonomousPermissions,
+          ev.grantedCommandPermissions
+        );
 
-      await withPool(async () => {
-        await saveInstallation(location, record);
-      });
+        await withPool(async () => {
+          await saveInstallation(location, record);
+        });
+      }
+      if (ev.kind === "bot_uninstalled_event") {
+        console.log("uninstalling: ", ev.location);
+        const location = ev.location;
+        await withPool(async () => {
+          await uninstall(location);
+        });
+      }
+      return {
+        statusCode: 200,
+      };
+    },
+    (error) => {
+      console.error("Bot event parsing failed", error);
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: "Failed to parse bot event",
+          error,
+        }),
+      };
     }
-    if (result.event.kind === "bot_uninstalled_event") {
-      console.log("uninstalling: ", result.event.location);
-      const location = result.event.location;
-      await withPool(async () => {
-        await uninstall(location);
-      });
-    }
-  } else if (result.kind === "bot_event_parse_failure") {
-    console.error("Bot event parsing failed", result);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: "Failed to parse bot event",
-        result,
-      }),
-    };
-  }
-  return {
-    statusCode: 200,
-  };
+  );
 };
